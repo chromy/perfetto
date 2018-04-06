084add9818f93737300e8307229eb239220060a0
Hector Dearman <hjd@google.com>
2018-04-06 18:01:46 +0100

    more

--- ui/perfetto.js
+++ ui/perfetto.js
@@ -12,23 +12,85 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+class Config {
+  constructor(name) {
+    this.name = name;
+  }
+}
 
-let Home = {
+let Configs = [
+  new Config('ftrace.cfg'),
+  new Config('atrace.cfg'),
+];
+
+class Home {
+  constructor() {
+    this.state = 'overview';
+  }
+
+  createNewConfig() {
+    this.state = 'edit';
+    console.log('createNewConfig');
+  }
+}
+
+let TheHome = new Home();
+
+function ConfigListView(home) {
+  return m(".mixer-column", [
+    m('h1', 'Configs'),
+    m('ul.big-list', 
+      m('li.help-text', 'Create, edit and manage configs here.'),
+      m('li.inset', 'Drag and drop or ', m('a[href=""]', 'click to import'), '.'),
+//      m('li.card', {onclick: () => home.createNewConfig()}, 'Create new config'),
+      m('li.big-list-item', BigListItemView(
+          m('.help-text', 'New Config'),
+          m('.help-text', 'Details about the config will appear here'), 'create')),
+      Configs.map(c => ConfigListItemView(c))),
+  ]);
+}
+
+function ConfigListItemView(config) {
+  return BigListItemView(
+      config.name,
+      ['12kb', '7 buffers'].join(' - '),
+      'edit');
+}
+
+function BigListItemView(name, body, action) {
+  return m('li.big-list-item', [
+      m('.big-list-item-header', name),
+      m('.big-list-item-body', body),
+      action ? m('.big-list-item-action', action) : null,
+  ]);
+}
+
+function ConfigEditorView() {
+  return m('h1', 'Editing');
+}
+
+let HomeComponent = {
   view: function() {
-    return m("#mixer", [
-        m(".mixer-column", [
-          m('h1', 'Configs'),
-        ]),
-        m(".mixer-column", [
-          m('h1', 'Devices'),
-        ]),
-        m(".mixer-column", [
-          m('h1', 'Traces'),
-        ]),
-    ])
+    home = TheHome;
+    if (home.state === 'overview')
+      return m("#mixer", [
+          ConfigListView(home),
+          m(".mixer-column", [
+            m('h1', 'Devices'),
+          ]),
+          m(".mixer-column", [
+            m('h1', 'Traces'),
+          ]),
+      ]);
+
+    if (home.state === 'edit')
+      return m("#mixer", [
+          ConfigListView(home),
+          m('.mixer-config-editor', ConfigEditorView()),
+      ]);
   }
 };
 
-m.route(document.body, '/', {
-  '/': Home,
+m.route(document.querySelector('main'), '/', {
+  '/': HomeComponent,
 });
