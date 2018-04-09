00fccff78569bb5d3a9f47c940a4723134f3c33f
Hector Dearman <hjd@google.com>
2018-04-09 17:42:13 +0100

    more

--- ui/perfetto.ts
+++ ui/perfetto.ts
@@ -12,8 +12,29 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import * as m from 'mithril';
+import * as protobuf from "protobufjs";
+
+
+let json_descriptor = require("./perfetto_config.json");
+let root = protobuf.Root.fromJSON(json_descriptor);
+let TraceConfig = root.lookupType("perfetto.TraceConfig");
+
+console.log(json_descriptor);
+console.log(root);
+console.log(TraceConfig);
+
 class Config {
-  constructor(name) {
+  name: string;
+  data: protobuf.Message<{}>;
+
+  constructor(name: string) {
+    this.name = name;
+    this.data = TraceConfig.create();
+  }
+
+  setName(name: string) {
+    console.log("name is ", name, this.name);
     this.name = name;
   }
 }
@@ -24,70 +45,147 @@ let Configs = [
 ];
 
 class Home {
+  focused_config: Config | null;
+  new_config: Config;
+
   constructor() {
-    this.state = 'overview';
-  }
+    this.focused_config = null;
+    this.new_config = new Config("");
+  };
 
   createNewConfig() {
-    this.state = 'edit';
-    console.log('createNewConfig');
+    this.focused_config = new Config("");
+  }
+
+  isEditingConfig() {
+    return this.focused_config !== null;
+  }
+
+  editConfig(config: Config) {
+    this.focused_config = config;
+  }
+
+  stopEditingConfig() {
+    this.focused_config = null;
   }
 }
 
 let TheHome = new Home();
 
 function ConfigListView(home) {
+  function ConfigListItemView(config) {
+    let name = config.name;
+    let action = m('.big-list-item-action', {onclick: () => home.editConfig(config)}, "edit");
+    let summary = ['12kb', '7 buffers'].join(' - ');
+
+    if (config === home.new_config) {
+      name = m('.help-text', config.name ? config.name : 'New Config');
+      action = m('.big-list-item-action', {onclick: () => home.editConfig(config)}, "create");
+      summary = m('.help-text', 'Details about the config will appear here');
+
+    }
+
+    if (config === home.focused_config)
+      action = m('.big-list-item-action', {onclick: () => home.stopEditingConfig()}, "close")
+    return m('li.big-list-item', [
+        m('.big-list-item-header', name),
+        m('.big-list-item-body', summary),
+        action,
+    ]);
+  }
+
   return m(".mixer-column", [
     m('h1', 'Configs'),
-    m('ul.big-list', 
+    m('ul.big-list',
       m('li.help-text', 'Create, edit and manage configs here.'),
       m('li.inset', 'Drag and drop or ', m('a[href=""]', 'click to import'), '.'),
-//      m('li.card', {onclick: () => home.createNewConfig()}, 'Create new config'),
-      m('li.big-list-item', BigListItemView(
-          m('.help-text', 'New Config'),
-          m('.help-text', 'Details about the config will appear here'), 'create')),
+      //m('li.big-list-item', BigListItemView(
+      ConfigListItemView(home.new_config),
       Configs.map(c => ConfigListItemView(c))),
   ]);
 }
 
-function ConfigListItemView(config) {
-  return BigListItemView(
-      config.name,
-      ['12kb', '7 buffers'].join(' - '),
-      'edit');
-}
+function ConfigEditorView(config) {
+  let text = '';
+  let proto_type = TraceConfig;
+  console.log(config.data);
+  text += TraceConfig.name;
+  text += '\n';
+  for (let field of Object.values(proto_type.fields)) {
+    text += '  ' +  field.name + " (" + field.type + ")\n";
+  }
 
-function BigListItemView(name, body, action) {
-  return m('li.big-list-item', [
-      m('.big-list-item-header', name),
-      m('.big-list-item-body', body),
-      action ? m('.big-list-item-action', action) : null,
-  ]);
-}
+  let fields = [];
+  for (let field of Object.values(proto_type.fields)) {
+    let name = field.name;
+    let type = field.type;
+    let is_repeated = field.repeated;
+    if (type === 'string') {
+      fields.push(
+        m('label', [
+          field.name,
+          m('input', {
+            oninput: m.withAttr('value', v => { config.data[name] = v }),
+            value: config.data[name],
+          })
+        ])
+      );
+    }
+    if (type === 'bool') {
+      fields.push(
+        m('label', [
+          field.name,
+          m('input', {
+            oninput: m.withAttr('value', v => { config.data[name] = v }),
+            value: config.data[name],
+          })
+        ])
+      );
+    }
+    if (type === 'uint32' || type === 'uint64') {
+      fields.push(
+        m('label', [
+          field.name,
+          m('input', {
+            oninput: m.withAttr('value', v => { config.data[name] = v }),
+            value: config.data[name],
+          })
+        ])
+      );
+    }
+  }
 
-function ConfigEditorView() {
-  return m('h1', 'Editing');
+  return [
+    m('h1', 'Editing'),
+    m('input.big-input', {
+      oninput: m.withAttr('value', name => { config.name = name }),
+      value: config.name,
+    }),
+    m('pre', text),
+    fields,
+  ];
 }
 
 let HomeComponent = {
   view: function() {
-    home = TheHome;
-    if (home.state === 'overview')
+    let home = TheHome;
+    if (home.isEditingConfig()) {
+      let config = home.focused_config;
       return m("#mixer", [
           ConfigListView(home),
-          m(".mixer-column", [
-            m('h1', 'Devices'),
-          ]),
-          m(".mixer-column", [
-            m('h1', 'Traces'),
-          ]),
+          m('.mixer-config-editor', ConfigEditorView(config)),
       ]);
+   }
 
-    if (home.state === 'edit')
-      return m("#mixer", [
-          ConfigListView(home),
-          m('.mixer-config-editor', ConfigEditorView()),
-      ]);
+    return m("#mixer", [
+        ConfigListView(home),
+        m(".mixer-column", [
+          m('h1', 'Devices'),
+        ]),
+        m(".mixer-column", [
+          m('h1', 'Traces'),
+        ]),
+    ]);
   }
 };
 
