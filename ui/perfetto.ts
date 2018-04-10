275d5e68d086ff35ee59beed90bff609c1e642bb
Hector Dearman <hector.dearman@gmail.com>
2018-04-10 01:48:00 +0100

    more

--- ui/perfetto.ts
+++ ui/perfetto.ts
@@ -15,7 +15,6 @@
 import * as m from 'mithril';
 import * as protobuf from "protobufjs";
 
-
 let json_descriptor = require("./perfetto_config.json");
 let root = protobuf.Root.fromJSON(json_descriptor);
 let TraceConfig = root.lookupType("perfetto.TraceConfig");
@@ -37,6 +36,11 @@ class Config {
     console.log("name is ", name, this.name);
     this.name = name;
   }
+
+  details() {
+    let num_buffers = this.data['buffers'].length;
+    return [num_buffers + ' buffers'];
+  }
 }
 
 let Configs = [
@@ -51,6 +55,7 @@ class Home {
   constructor() {
     this.focused_config = null;
     this.new_config = new Config("");
+    this.focused_config = Configs[0];
   };
 
   createNewConfig() {
@@ -76,13 +81,12 @@ function ConfigListView(home) {
   function ConfigListItemView(config) {
     let name = config.name;
     let action = m('.big-list-item-action', {onclick: () => home.editConfig(config)}, "edit");
-    let summary = ['12kb', '7 buffers'].join(' - ');
+    let summary = m('span', config.details().join(' - '));
 
     if (config === home.new_config) {
       name = m('.help-text', config.name ? config.name : 'New Config');
       action = m('.big-list-item-action', {onclick: () => home.editConfig(config)}, "create");
       summary = m('.help-text', 'Details about the config will appear here');
-
     }
 
     if (config === home.focused_config)
@@ -105,6 +109,99 @@ function ConfigListView(home) {
   ]);
 }
 
+function ProtoEditorFieldsView(type, proto) {
+  let fields = [];
+  for (let field of Object.values(type.fields)) {
+    fields.push(ProtoEditorFieldView(field, proto));
+  }
+  return fields;
+}
+
+function ProtoEditorFieldView(field, parent) {
+  field.resolve();
+  console.log('parent', parent);
+  console.log(field);
+  console.log(field.name);
+  console.log(field.resolvedType);
+  if (field.repeated)
+    return null;
+  if (field.type === 'string') 
+    return ProtoEditorStringView(field, parent);
+  if (field.type === 'bool') 
+    return ProtoEditorBoolView(field, parent);
+  if (['uint32', 'uint64', 'int32', 'int64'].indexOf(field.type) != -1)
+    return ProtoEditorNumberView(field, parent);
+  if (field.resolvedType.values != undefined)
+    return ProtoEditorEnumView(field, parent);
+  return ProtoEditorNestedView(field, parent);
+}
+
+function ProtoEditorEnumView(field, parent) {
+  console.log('enum');
+  return m('div');
+}
+
+function ProtoEditorNestedView(field, parent) {
+  //  let type = root.lookupType('perfetto.' + field.type);
+  console.log('ProtoEditorNestedView', field, parent);
+  let type = field.resolvedType;
+  let child = parent[field.name];
+  if (child === null) {
+    return m('.editor', [
+      m('label', field.name),
+      m('button', {
+        onclick: () => parent[field.name] = type.create(),
+      }, 'add'),
+    ]);
+  }
+
+  return m('.editor.column', [
+    m('label', field.name),
+    m('.nest', ProtoEditorFieldsView(type, child)),
+  ]);
+}
+
+function ProtoEditorBoolView(field, parent) {
+  console.log(field.name, parent[field.name]);
+
+  let values = [undefined, true, false];
+  let names = ['unset', 'true', 'false'];
+
+  let value = parent[field.name];
+  let value_name = names[values.indexOf(value)];
+  let next_value = values[(values.indexOf(value) + 1) % names.length];
+
+  return m('.editor', [
+    m('label', field.name),
+    m('button', {
+      onclick: () => parent[field.name] = next_value,
+    }, value_name),
+  ]);
+}
+
+function ProtoEditorStringView(field, parent) {
+  console.log("String View", field, parent);
+  return m('.editor', [
+    m('label', field.name),
+    m('input', {
+      oninput: m.withAttr('value', v => parent[field.name] = v),
+      value: parent[field.name],
+    })
+  ]);
+}
+
+function ProtoEditorNumberView(field, parent, hint = null) {
+  console.log('ProtoEditorNumberView', field, parent);
+  return m('.editor', [
+    m('label', field.name),
+    hint ? hint(parent[field.name]) : null,
+    m('input', {
+      oninput: m.withAttr('value', v => parent[field.name] = v),
+      value: parent[field.name],
+    }),
+  ]);
+}
+
 function ConfigEditorView(config) {
   let text = '';
   let proto_type = TraceConfig;
@@ -117,42 +214,7 @@ function ConfigEditorView(config) {
 
   let fields = [];
   for (let field of Object.values(proto_type.fields)) {
-    let name = field.name;
-    let type = field.type;
-    let is_repeated = field.repeated;
-    if (type === 'string') {
-      fields.push(
-        m('label', [
-          field.name,
-          m('input', {
-            oninput: m.withAttr('value', v => { config.data[name] = v }),
-            value: config.data[name],
-          })
-        ])
-      );
-    }
-    if (type === 'bool') {
-      fields.push(
-        m('label', [
-          field.name,
-          m('input', {
-            oninput: m.withAttr('value', v => { config.data[name] = v }),
-            value: config.data[name],
-          })
-        ])
-      );
-    }
-    if (type === 'uint32' || type === 'uint64') {
-      fields.push(
-        m('label', [
-          field.name,
-          m('input', {
-            oninput: m.withAttr('value', v => { config.data[name] = v }),
-            value: config.data[name],
-          })
-        ])
-      );
-    }
+    fields.push(ProtoEditorFieldView(field, config.data));
   }
 
   return [
@@ -161,8 +223,7 @@ function ConfigEditorView(config) {
       oninput: m.withAttr('value', name => { config.name = name }),
       value: config.name,
     }),
-    m('pre', text),
-    fields,
+    m('.fields', fields),
   ];
 }
 
