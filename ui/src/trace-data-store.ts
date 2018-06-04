0005772b1a250813fae9155bd4f5f18897906eda
Deepanjan Roy <dproy@google.com>
2018-06-04 13:39:57 -0400

    Basic TDS implementation

--- ui/src/trace-data-store.ts
+++ ui/src/trace-data-store.ts
@@ -1,9 +1,55 @@
-class TraceDataStore {
-  getData(query: {process: string, thread: string, start: number, end: number}) {
-    // Use query to get some data
-    console.log(query);
+interface ThreadSlice {
+  start: number,
+  end: number
+  title: string,
+  tid: number,
+  pid: number,
+};
+
+type ThreadDataMap = Map<number, ThreadSlice[]>;
+type ProcessDataMap = Map<number, ThreadDataMap>;
+
+function getFirstIndexWhereTrue(slices: ThreadSlice[],
+    testFn: (slice: ThreadSlice) => Boolean) : number {
+  // TODO: This should be a binary search.
+  for (let i = 0; i < slices.length; i++) {
+    if (testFn(slices[i])) return i;
   }
+  return slices.length;
+}
+
+export class TraceDataStore {
+  // TODO: This is super ad hoc. Figure out how to actually cache data properly.
+  // We need to figure out what the cache keys should be, and how data
+  // expiration will work (LRU?)
+  private data: ProcessDataMap;
+
+  constructor() {
+    this.data = new Map();
+  }
+
+  * getData(query: TraceDataQuery) {
+    if (query.process === undefined || query.thread === undefined) return;
+    const process = query.process;
+    const threadData = this.data.get(process);
+    if (threadData == null) return;
+    const slices = threadData.get(query.thread);
+    if (slices == null || slices.length == 0) return;
+    const beginIndex = getFirstIndexWhereTrue(slices, s => query.start < s.end);
+    const endIndex  = getFirstIndexWhereTrue(slices, s => query.end < s.start);
+    for (let i = beginIndex; i < endIndex; i++) {
+      yield slices[i];
+    }
+  }
+
   onNewDataReceived() {
     //App.render();
   }
+}
+
+export interface TraceDataQuery {
+  readonly start: number;
+  readonly end: number;
+  readonly process?: number;
+  readonly thread?: number;
 }
