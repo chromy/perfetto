9771546f999ff796af170acf2780296bfa34b771
Michail Schwab <michaschwab@google.com>
2018-06-14 15:05:57 -0400

    Some styling on track shells

--- ui/src/track-shell.ts
+++ ui/src/track-shell.ts
@@ -33,26 +33,38 @@ export class TrackShell extends LitElement {
       height: ${this.height}px;
       box-sizing: border-box;
       position: relative;
-      border-bottom: 1px solid #000;
+      border-bottom: 1px solid #999;
     }
     .shell-content {
       background:#eee;
       box-sizing: border-box;
-      padding: 20px;
-      width: ${this.shellWidth}px;
-      height: ${this.height-1}px;
+      padding: 10px 20px;
+      width: ${this.shellWidth - 1}px;
+      height: ${this.height - 1}px;
       display: block;
+      border-left: 8px solid #36aa89;
+      border-right: 1px solid #999;
     }
     .track-content {
       position: absolute;
       top: ${this.contentPosition.top}px;
       left: ${this.contentPosition.left}px; 
-      width: ${this.width-this.contentPosition.left-this.contentPosition.right}px;
-      height: ${this.height-this.contentPosition.top-this.contentPosition.bottom}px;
+      width: ${this.width - this.contentPosition.left - this.contentPosition.right}px;
+      height: ${this.height - this.contentPosition.top - this.contentPosition.bottom}px;
+    }
+    ul {
+      padding-left: 30px;
+      
     }
     </style>
     <div class="wrap">
-      <div class="shell-content">${this.name}</div>
+      <div class="shell-content">
+        <b>${this.name}</b>
+        <ul>
+          <li>Some Important</li>
+          <li>Information</li>
+        </ul>
+      </div>
       <div class="track-content">
         <slot></slot>
       </div>
